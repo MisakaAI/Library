23d415b76999d3ecc8242381ea764a2fde40ce1d
MisakaAI <i@zi-o.com>
2023-04-26 16:06:24 +0800

    change chmod.md

--- Linux/chmod.sh
+++ Linux/chmod.sh
@@ -4,8 +4,10 @@
 
 # 查找目录修改目录权限为755
 # find . -type d -exec chmod 755 {} \;
+# find /desired_location -type d -print0 | xargs -0 chmod 0755
 find . -type d | xargs chmod 755
 
 #　查找文件修改文件权限为644
 # find . -type f -exec chmod 644 {} \;
+# find /desired_location -type f -print0 | xargs -0 chmod 0644
 find .  -type f | xargs chmod 644
